cfefb30af9e40d4cce7a2cc9fd37ea254d882107
gustavo.moraes <gustavo.moraes@valemobi.com.br>
2022-04-10 21:21:12 -0300

    build(java-swing): created frontend

---
+++ java-swing/magna-monitoring-system/src/main/java/com/magna/MagnaMonitoringSystem.java
@@ -0,0 +1,11 @@
+package com.magna;
+
+import com.magna.signup.LoginScreen;
+
+public class MagnaMonitoringSystem {
+
+    public static void main(String[] args) {
+        LoginScreen frame1 = new LoginScreen();
+        frame1.setVisible(true);
+    }
+}
