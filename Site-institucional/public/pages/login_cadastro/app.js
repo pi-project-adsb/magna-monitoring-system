95a5a6d1b280a5e5f0465001b64578178d1b9788
LucasOs4kwe <LUCAS.SSANTOS@bandtec.com.br>
2022-04-26 13:02:54 -0300

    feat(login_cadastro.html): login/signup animation added

--- Site-institucional/public/pages/login_cadastro/app.js
+++ Site-institucional/public/pages/login_cadastro/app.js
@@ -13,3 +13,31 @@ function checkPass(x) {
         inpt.type = 'password';
     }
 }
+
+function test(a) {
+    var login = document.getElementById('login-box');
+    var signUp = document.getElementById('signUp-box');
+    if (a == 1) {
+        if(login.classList == ''){
+            login.classList.add('form_hide');
+        }else{
+            login.classList.replace(`${login.classList}`,'form_hide');
+        }
+        setTimeout(() => {
+            login.style.display = 'none';
+            if(signUp.classList == ''){
+                signUp.classList.add('form_ap');
+            }else{
+                signUp.classList.replace(`${signUp.classList}`, 'form_ap');
+            }
+            signUp.style.display = 'block';
+        }, 500);
+    }else{
+        signUp.classList.replace('form_ap', 'form_hide');
+        setTimeout(() => {
+            signUp.style.display = 'none';
+            login.classList.replace('form_hide', 'form_ap');
+            login.style.display = 'block';
+        }, 500);
+    }
+}
