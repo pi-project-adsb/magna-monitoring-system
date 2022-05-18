072e727f8e1c18f43c00c5cd70c0e4e090dedd6a
Lucas Lacerda <89214092+Lucaszib@users.noreply.github.com>
2022-05-18 17:40:13 -0300

    build(model.sh): create model shellScript

---
+++ shell-script/model.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+#-VARIAVEIS INFO-----------------------------------------------------#
+
+NOME_PROGRAMA="$(basename $0 | cut -d. -f1)"
+VERSAO="1.0"
+AUTOR="Lucaszib"
+CONTATO="https://github.com/Lucaszib"
+DESCRICAO="Modelo base para outros scripts shellscript"
+varEXE=$1 # Se não tiver parametros ela executa normal
+
+#-VARIAVEIS PARAMETRO----------------------------------------------------#
+
+varINFO="
+Nome do Programa: $NOME_PROGRAMA
+Autor: $AUTOR
+Versão: $VERSAO
+Descrição do Programa: $DESCRICAO
+"
+varHELP="
+Instruções para Ajuda:
+	-h ou --help: Abre a ajuda de comandos do usuário;
+	-i ou --info: Informações sobre o programa;
+"
+
+#-TESTES--------------------------------------------------------------------------#
+
+
+
+#-LOOP PARA RODAR MAIS PARAMETROS---------------------------------------------------#
+
+while test -n "$1"; do
+
+	case $1 in
+
+		-i |  --info)  	echo "$varINFO" 											;;		
+		-h |  --help)  	echo "$varHELP"												;;
+		-d | --debug)	bash -x $0													;;
+				   *) 	echo "\nComando inválido. Digite -h ou --help para ajuda\n"	;;
+
+	esac
+	shift
+
+done
+#-FUNÇÕES--------------------------------------------------------------------------#
+
+
+
+#-EXECUÇÃO-------------------------------------------------------------------------#
+
+if [ -z "$varEXE" ]; then
+	# Coloca o main do programa aqui
+fi
